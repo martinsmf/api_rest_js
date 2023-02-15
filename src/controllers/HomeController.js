f64c9d314034832f4902071b2e647e2b8f7773a0
Matheus Martins <martinsmf00@gmail.com>
2023-02-15 15:41:45 -0300

    create user

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,8 +1,16 @@
+import Aluno from '../models/Aluno';
+
 class HomeController {
-  index(req, res) {
-    res.json({
-      tudoCerto: true,
+  async index(req, res) {
+    const novoAluno = await Aluno.create({
+      nome: 'Matheus',
+      sobrenome: 'Martins',
+      email: 'matheus@dev.com',
+      idade: 200,
+      peso: 70,
+      altura: 1.8,
     });
+    res.json(novoAluno);
   }
 }
 
