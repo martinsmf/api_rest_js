d600d79913c4a03a23fffacababe54a22ce673b6
Matheus Martins <martinsmf00@gmail.com>
2023-02-18 19:08:31 -0300

    .

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,5 +4,17 @@ import userController from '../controllers/UserController';
 const router = new Router();
 
 router.post('/', userController.store);
+router.get('/', userController.index);
+router.get('/:id', userController.show);
+router.put('/:id', userController.update);
+router.delete('/:id', userController.delete);
 
 export default router;
+
+/*
+  index -> lista todos os usuários -> GET
+  store/create -> cria um usuário -> POST
+  delete -> apaga um usuário -> DELETE
+  show -> mostra um usuário -> GET
+  update -> atualiza um usuário -> PATCH ou PUT
+*/
