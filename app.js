028ce300755a0fd6dbd19b61247dfaf5a1be1d68
Matheus Martins <martinsmf00@gmail.com>
2023-02-18 22:48:39 -0300

    .

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ dotenv.config();
 import './src/database';
 
 import express from 'express';
-import homeRoutes from './src/routes/homeRoutes';
-import userRoutes from './src/routes/userRoutes';
+import homeRoutes from './src/routes/home.routes';
+import userRoutes from './src/routes/user.routes';
+import tokenRoutes from './src/routes/token.routes';
 
 class App {
   constructor() {
@@ -23,6 +24,7 @@ class App {
   routes() {
     this.app.use('/', homeRoutes);
     this.app.use('/users/', userRoutes);
+    this.app.use('/tokens/', tokenRoutes);
   }
 }
 export default new App().app;
